a4d03a08b3f729577980dc1c993879b61f33e9f1
Somel <somelahmed88@gmail.com>
2023-10-05 19:35:47 +0600

    Q zone Added

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,4 +1,9 @@
 import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import qZone1 from "../../../assets/qZone1.png"
+import qZone2 from "../../../assets/qZone2.png"
+import qZone3 from "../../../assets/qZone3.png"
+
+
 const RightSideNav = () => {
   return (
     <div>
@@ -27,6 +32,15 @@ const RightSideNav = () => {
         <a className='flex items-center gap-3 p-4 font-extralight border rounded-b-lg ' href="">
         <FaInstagram></FaInstagram>
         Instagram</a>
+      </div>
+
+      {/* q Zone  */}
+      <div className='space-y-3 mb-6 bg-gray-100 p-3'>
+        <h2 className=" font-bold ">Q Zone</h2>
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
+        
       </div>
     </div>
   );
