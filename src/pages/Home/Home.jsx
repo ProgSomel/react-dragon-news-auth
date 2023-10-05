cfd829107b62d32e4ff55d6c315c994f397a2030
Somel <somelahmed88@gmail.com>
2023-10-05 19:30:18 +0600

    Find us on Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
                 <div className="md:col-span-2 border">
                     <h2 className="text-4xl ">News Comming Soon...........</h2>
                 </div>
-                <div className="border">
+                <div className="">
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,10 +1,35 @@
-
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
 const RightSideNav = () => {
-    return (
-        <div>
-            <h2 className="text-2xl">Right Side</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className='space-y-3 mb-6'>
+        <h2 className="text-2xl font-bold ">login with</h2>
+        <button className="btn btn-outline w-full text-blue-400">
+        <FaGoogle></FaGoogle>
+          Login with Google
+        </button>
+        <button className="btn btn-outline w-full ">
+        <FaGithub></FaGithub>
+          Login with Github
+        </button>
+      </div>
+      {/* Find Us on  */}
+      <div className=' mb-8'>
+        <h2 className=" font-bold mb-4 ">Find Us On</h2>
+        <a className='flex items-center gap-3 p-4 font-extralight border rounded-t-lg' href="">
+        <FaFacebook></FaFacebook>
+        Facebook</a>
+
+        <a className='flex items-center gap-3 p-4 font-extralight border ' href="">
+        <FaTwitter></FaTwitter>
+        Twitter</a>
+
+        <a className='flex items-center gap-3 p-4 font-extralight border rounded-b-lg ' href="">
+        <FaInstagram></FaInstagram>
+        Instagram</a>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
